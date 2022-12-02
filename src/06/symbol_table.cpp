f7ec1eb2f8d2c59340f0dd3bb17e9fbd9dfc51fc
dl10yr <tkzw04202166@gmail.com>
2022-12-02 22:59:09 +0900

    fix symbol_table

--- src/06/symbol_table.cpp
+++ src/06/symbol_table.cpp
@@ -1,4 +1,5 @@
 #include "./symbol_table.hpp"
+#include "./error.hpp"
 #include <string>
 
 using namespace std;
@@ -8,6 +9,17 @@ namespace assembler {
 
 void SymbolTable::AddEntry(string s, int addr) {
   _map[s] = addr;
+}
+
+bool SymbolTable::Contains(string s) {
+  return _map.count(s) != 0;
+}
+
+int SymbolTable::GetAddress(string s) {
+  if (!Contains(s)) {
+    throw UndefinedSymbolException(string("Undefined symbol: ") + s);
+  }
+  return _map[s];
 }
 
 }
